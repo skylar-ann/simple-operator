7defea8a9259deb41ae7c67b733bf18e50b150ab
Matt Simons <matthew.simons@worldpay.com>
2021-09-10 11:49:37 +0100

    initial

---
+++ controllers/simple_controller_test.go
@@ -0,0 +1,89 @@
+package controllers
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	simplev1alpha1 "github.worldpay.com/Atlas/simple-operator/api/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+var _ = Describe("SimpleReconciler", func() {
+
+	Context("when reconciling a Simple with no starting spec", func() {
+		var (
+			ctx             = context.Background()
+			simpleObjectKey = client.ObjectKey{Name: "simple", Namespace: "basic-test"}
+
+			namespace = &corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: simpleObjectKey.Namespace}}
+			simple    = &simplev1alpha1.Simple{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      simpleObjectKey.Name,
+					Namespace: simpleObjectKey.Namespace,
+				},
+			}
+		)
+
+		Specify("a Simple resource exists", func() {
+			Expect(k8sClient.Create(ctx, namespace)).To(Succeed())
+			Expect(k8sClient.Create(ctx, simple)).To(Succeed())
+		})
+
+		It("should create a configmap with no data", func() {
+			cm := &corev1.ConfigMap{}
+
+			Eventually(func() error {
+				return k8sClient.Get(ctx, simpleObjectKey, cm)
+			}, time.Second*3, time.Millisecond*500).Should(Succeed())
+
+			Expect(cm.Data).Should(BeEmpty())
+		})
+
+		It("should reconcile the configmap when foo is added", func() {
+			simple.Spec.Foo = "newdata"
+
+			Eventually(func() error {
+				return k8sClient.Update(ctx, simple)
+			}, time.Second*3, time.Millisecond*500).Should(Succeed())
+
+			cm := &corev1.ConfigMap{}
+
+			Eventually(func() map[string]string {
+				k8sClient.Get(ctx, simpleObjectKey, cm)
+				return cm.Data
+			}, time.Second*3, time.Millisecond*500).Should(
+				HaveKeyWithValue("something.conf", fmt.Sprintf("setting = %s", simple.Spec.Foo)),
+			)
+		})
+
+		It("should reconcile the configmap when it is tampered", func() {
+			cm := &corev1.ConfigMap{}
+
+			Eventually(func() error {
+				return k8sClient.Get(ctx, simpleObjectKey, cm)
+			}, time.Second*3, time.Millisecond*500).Should(Succeed())
+
+			// Tamper Simple's configmap
+			cm.Data["something.conf"] = "setting = tampered"
+			Eventually(func() error {
+				return k8sClient.Update(ctx, cm)
+			}, time.Second*3, time.Millisecond*500).Should(Succeed())
+
+			// The value should be restored automatically
+			Eventually(func() map[string]string {
+				k8sClient.Get(ctx, simpleObjectKey, cm)
+				return cm.Data
+			}, time.Second*3, time.Millisecond*500).Should(
+				HaveKeyWithValue("something.conf", fmt.Sprintf("setting = %s", simple.Spec.Foo)),
+			)
+		})
+
+		It("should reconcile the configmap when it is deleted", func() {
+		})
+	})
+})
---
+++ controllers/suite_test.go
@@ -0,0 +1,113 @@
+/*
+Copyright 2021.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	"path/filepath"
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
+	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	simplev1alpha1 "github.worldpay.com/Atlas/simple-operator/api/v1alpha1"
+	//+kubebuilder:scaffold:imports
+)
+
+// These tests use Ginkgo (BDD-style Go testing framework). Refer to
+// http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
+
+var cfg *rest.Config
+var k8sClient client.Client
+var testEnv *envtest.Environment
+var shutdownControllers context.CancelFunc
+
+func TestAPIs(t *testing.T) {
+	RegisterFailHandler(Fail)
+
+	RunSpecsWithDefaultAndCustomReporters(t,
+		"Controller Suite",
+		[]Reporter{printer.NewlineReporter{}})
+}
+
+var _ = BeforeSuite(func() {
+	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
+
+	By("bootstrapping test environment")
+	testEnv = &envtest.Environment{
+		CRDDirectoryPaths:     []string{filepath.Join("..", "config", "crd", "bases")},
+		ErrorIfCRDPathMissing: true,
+	}
+
+	cfg, err := testEnv.Start()
+	Expect(err).NotTo(HaveOccurred())
+	Expect(cfg).NotTo(BeNil())
+
+	err = simplev1alpha1.AddToScheme(scheme.Scheme)
+	Expect(err).NotTo(HaveOccurred())
+
+	//+kubebuilder:scaffold:scheme
+
+	// Create the manager with controllers
+	mgr, err := ctrl.NewManager(cfg, ctrl.Options{})
+	Expect(mgr).ToNot(BeNil())
+	Expect(err).NotTo(HaveOccurred(), "failed to create manager")
+
+	err = (&SimpleReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr)
+	Expect(err).NotTo(HaveOccurred(), "failed to setup controller")
+
+	//+kubebuilder:scaffold:scheme
+
+	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
+	Expect(err).NotTo(HaveOccurred())
+	Expect(k8sClient).NotTo(BeNil())
+
+	// We will use this channel to shutdown the operator after tests complete
+	// See AfterSuite function
+	var ctx context.Context
+	ctx, shutdownControllers = context.WithCancel(context.Background())
+
+	// Start our operator
+	go func() {
+		err := mgr.Start(ctx)
+		Expect(err).NotTo(HaveOccurred(), "failed to start manager")
+	}()
+
+}, 60)
+
+var _ = AfterSuite(func() {
+	By("tearing down the test environment")
+	err := testEnv.Stop()
+	Expect(err).NotTo(HaveOccurred())
+
+	// Shutdown our operator
+	shutdownControllers()
+
+	// Shutdown the control plane
+	Expect(testEnv.Stop()).Should(Succeed())
+})
